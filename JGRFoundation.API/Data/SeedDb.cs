bbc26b91174e7af4f19ec5ea02bbf80090c18096
ricardogomez86268 <ricardogomez86268@correo.itm.edu.co>
2023-05-24 23:35:57 -0500

    Se ajustan las pruebas, se ordenan, se eliminan pruebas de categoria

--- JGRFoundation.API/Data/SeedDb.cs
+++ JGRFoundation.API/Data/SeedDb.cs
@@ -1,24 +1,20 @@
-﻿using System.Runtime.InteropServices;
-using Microsoft.EntityFrameworkCore;
-using JGRFoundation.API.Helpers;
+﻿using JGRFoundation.API.Helpers;
 using JGRFoundation.API.Services;
 using JGRFoundation.Shared.Entities;
 using JGRFoundation.Shared.Enums;
-using JGRFoundation.Shared.Responses;
+using System.Runtime.InteropServices;
 
 namespace JGRFoundation.API.Data
 {
     public class SeedDb
     {
         private readonly DataContext _context;
-        private readonly IApiService _apiService;
         private readonly IUserHelper _userHelper;
         private readonly IFileStorage _fileStorage;
 
         public SeedDb(DataContext context, IApiService apiService, IUserHelper userHelper, IFileStorage fileStorage)
         {
             _context = context;
-            _apiService = apiService;
             _userHelper = userHelper;
             _fileStorage = fileStorage;
         }
@@ -26,13 +22,14 @@ namespace JGRFoundation.API.Data
         public async Task SeedAsync()
         {
             await _context.Database.EnsureCreatedAsync();
-            await CheckCategoriesAsync();
+            await CheckRolesAsync();
+            await CheckBatteriesAsync();
+            await CheckHomeAppliancesAsync();
             await CheckInvestorsAsync();
             await CheckPanelsAsync();
-            await CheckRolesAsync();
-            await CheckUserAsync("1010", "Ricardo", "Gómez Gallego", "Rico@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "admin.jpeg", UserType.Admin);
-            await CheckUserAsync("1010", "Juan Guillermo", "Palma Cerón", "Juan@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "admin.jpeg", UserType.Admin);
-            await CheckUserAsync("1010", "Guillermo Herney", "Carrillo Rivera", "Guillermo@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "admin.jpeg", UserType.Admin);
+            await CheckUserAsync("1010", "Ricardo", "Gómez Gallego", "Ricardo@yopmail.com", "3015927246", "Calle xxx", "admin.jpeg", UserType.Admin);
+            await CheckUserAsync("1010", "Juan Guillermo", "Palma Cerón", "Juan@yopmail.com", "3223114620", "Calle Luna  ", "admin.jpeg", UserType.Admin);
+            await CheckUserAsync("1010", "Guillermo Herney", "Carrillo Rivera", "Guillermo@yopmail.com", "3223114620", "Calle Sol", "admin.jpeg", UserType.Admin);
         }
         private async Task<User> CheckUserAsync(string document, string firstName, string lastName, string email, string phone, string address, string image, UserType userType)
         {
@@ -76,14 +73,23 @@ namespace JGRFoundation.API.Data
             return user;
         }
 
-        private Task CheckInvestorsAsync()
+        private Task CheckBatteriesAsync()
         {
-            if (!_context.Investors.Any())
+            if (!_context.Batteries.Any())
             {
-                _context.Investors.AddRange(new List<Investor>()
+                _context.Batteries.AddRange(new List<Battery>()
                 {
-                    new Investor() { InvestorReference = "Invest1200" ,RatedPower = 1200},
-                    new Investor() { InvestorReference = "Invest3400" ,RatedPower = 3400}
+                    new Battery() { BatteryReference = "BatSolar100",Voltage=100, CapacityAmperageHour = 12 },
+                    new Battery() { BatteryReference = "BatSolar200",Voltage=200, CapacityAmperageHour = 12 },
+                    new Battery() { BatteryReference = "BatSolar300",Voltage=300, CapacityAmperageHour = 12 },
+                    new Battery() { BatteryReference = "BatSolar400",Voltage=400, CapacityAmperageHour = 12 }
+                });
+            }
+
+            return Task.CompletedTask;
+        }
+
+
         private Task CheckHomeAppliancesAsync()
         {
             if (!_context.HomeAppliances.Any())
@@ -100,6 +106,20 @@ namespace JGRFoundation.API.Data
             return Task.CompletedTask;
         }
 
+        private Task CheckInvestorsAsync()
+        {
+            if (!_context.Investors.Any())
+            {
+                _context.Investors.AddRange(new List<Investor>()
+                {
+                    new Investor() { InvestorReference = "Invest1200" ,RatedPower = 1200},
+                    new Investor() { InvestorReference = "Invest3400" ,RatedPower = 3400}
+                });
+            }
+
+            return Task.CompletedTask;
+        }
+
         private Task CheckPanelsAsync()
         {
             if (!_context.Panels.Any())
@@ -109,51 +129,16 @@ namespace JGRFoundation.API.Data
                     new Panel() { PanelReference = "Panel400",Power = 400},
                     new Panel() { PanelReference = "Panel500",Power = 500 },
                     new Panel() { PanelReference = "Panel600",Power = 600 }
-        private Task CheckBatteriesAsync()
-        {
-            if (!_context.Batteries.Any())
-            {
-                _context.Batteries.AddRange(new List<Battery>()
-                {
-                    new Battery() { BatteryReference = "BatSolar100",Voltage=100, CapacityAmperageHour = 12 },
-                    new Battery() { BatteryReference = "BatSolar200",Voltage=200, CapacityAmperageHour = 12 },
-                    new Battery() { BatteryReference = "BatSolar300",Voltage=300, CapacityAmperageHour = 12 },
-                    new Battery() { BatteryReference = "BatSolar400",Voltage=400, CapacityAmperageHour = 12 }
                 });
             }
 
             return Task.CompletedTask;
         }
-        private async Task CheckCategoriesAsync()
-        {
-            if (!_context.Categories.Any())
-            {
-                _context.Categories.Add(new Category { Name = "Apple" });
-                _context.Categories.Add(new Category { Name = "Autos" });
-                _context.Categories.Add(new Category { Name = "Belleza" });
-                _context.Categories.Add(new Category { Name = "Calzado" });
-                _context.Categories.Add(new Category { Name = "Comida" });
-                _context.Categories.Add(new Category { Name = "Cosmeticos" });
-                _context.Categories.Add(new Category { Name = "Deportes" });
-                _context.Categories.Add(new Category { Name = "Erótica" });
-                _context.Categories.Add(new Category { Name = "Ferreteria" });
-                _context.Categories.Add(new Category { Name = "Gamer" });
-                _context.Categories.Add(new Category { Name = "Hogar" });
-                _context.Categories.Add(new Category { Name = "Jardín" });
-                _context.Categories.Add(new Category { Name = "Jugetes" });
-                _context.Categories.Add(new Category { Name = "Lenceria" });
-                _context.Categories.Add(new Category { Name = "Mascotas" });
-                _context.Categories.Add(new Category { Name = "Nutrición" });
-                _context.Categories.Add(new Category { Name = "Ropa" });
-                _context.Categories.Add(new Category { Name = "Tecnología" });
-                await _context.SaveChangesAsync();
-            }
-        }      
-       
+
         private async Task CheckRolesAsync()
         {
             await _userHelper.CheckRoleAsync(UserType.Admin.ToString());
             await _userHelper.CheckRoleAsync(UserType.User.ToString());
-        }       
+        }
     }
 }
