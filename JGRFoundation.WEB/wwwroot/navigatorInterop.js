da00bb713860a68e2fb46ec03861b59d50830f2a
ricardogomez86268 <ricardogomez86268@correo.itm.edu.co>
2023-05-26 17:22:43 -0500

    Pruebas coordenada

---
+++ JGRFoundation.WEB/wwwroot/navigatorInterop.js
@@ -0,0 +1,13 @@
+﻿window.navigatorInterop = {
+    getCurrentPosition: function (dotNetObjectReference) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                dotNetObjectReference.invokeMethodAsync('HandlePosition', position.coords.latitude, position.coords.longitude);
+            }, function (error) {
+                dotNetObjectReference.invokeMethodAsync('HandleError', error.message);
+            });
+        } else {
+            dotNetObjectReference.invokeMethodAsync('HandleError', 'La geolocalización no es compatible en este navegador.');
+        }
+    }
+};
